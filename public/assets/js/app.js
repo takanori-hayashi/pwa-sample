bab5e3dea104e4f78bcdccfa6e760efdc6a75b14
takanori-hayashi <tk_hayashi@e-create.jp>
2019-05-15 18:12:28 +0900

    パス修正

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js')
+  navigator.serviceWorker.register('/assets/js/service-worker.js')
     .then(() => {
       console.log('Service worker registered!');
     });
