ff2ee39034b5e23eadb08fc2462f4d90c376b2bf
takanori-hayashi <tk_hayashi@e-create.jp>
2019-05-15 18:17:54 +0900

    エラー処理追加

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/assets/js/service-worker.js')
-    .then(() => {
-      console.log('Service worker registered!');
+    .then(reg => {
+      console.log(`Service worker registered! scope is ${reg.scope}`);
+    }).catch(error => {
+      console.log(`Registration failed with ${error}`);
     });
 }
---
+++ public/assets/js/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+
+self.addEventListener('fetch', event => {
+  console.log('service worker');
+});
