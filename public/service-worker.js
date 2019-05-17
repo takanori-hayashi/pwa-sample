6c7f7824985b3b18f391b708aba343fa0719520b
takanori-hayashi <tk_hayashi@e-create.jp>
2019-05-17 17:32:18 +0900

    キャッシュ処理追加

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,24 @@
-const CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+const STATIC_DATA = [
+  'index.html',
+  '/assets/js/app.js',
+  '/assets/images/image2.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cache_v1')
+      .then(cache => {
+        return cache.addAll(STATIC_DATA);
+      })
+  );
+});
 
 self.addEventListener('fetch', event => {
-  console.log('service worker');
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
