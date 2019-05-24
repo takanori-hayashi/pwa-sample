c032de75ae51c13908e83ba9cb9f34258e836a8f
takanori-hayashi <tk_hayashi@e-create.jp>
2019-05-24 14:52:48 +0900

    fetch処理追加

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,3 +15,16 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
